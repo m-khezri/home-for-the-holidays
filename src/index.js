04521196f2919d7a95794c4d68d29cd586eabb1e
mohammad <khezri@outlook.com>
2018-11-13 19:03:43 -0600

    dobe navbar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 // import $ from 'jquery';
-// import 'bootstrap';
 // import firebase from 'firebase';
 // import apiKeys from '../db/apiKeys.json';
+import 'bootstrap';
+import './index.scss';
+import createNavbar from './components/Navbar/navbar';
+
+const initilizerApp = () => {
+  createNavbar();
+};
+
+initilizerApp();
