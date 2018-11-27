884f8da285fc19d02c11f063259b84eed28f309b
mohammad <khezri@outlook.com>
2018-11-26 20:18:04 -0600

    holiday

--- src/components/FriendsPage/friendsPage.js
+++ src/components/FriendsPage/friendsPage.js
@@ -1,41 +1,61 @@
 import $ from 'jquery';
+// import { auth } from 'firebase';
 import authHelpers from '../../helpers/authHelpers';
 import friendsData from '../../helpers/data/friendsData';
+import holidayFriendsData from '../../helpers/data/holidayFriendsData';
+import holidaysData from '../../helpers/data/holidaysData';
 
+const holidayStringBuilder = (holidays) => {
+  let holidayString = '<h3>Holidays:</h3>';
+  holidays.forEach((holiday) => {
+    holidayString += `<h5>${holiday.name} ${holiday.Date}</h5>`;
+  });
+  return holidayString;
+};
 
-const printSingleFriend = (friend) => {
+const printSingleFriend = (friend, holidays) => {
   const friendString = `
-    <div>
-      <h1>${friend.name}</h1>
-      <h3>${friend.relationship}</h3>
-      <p>${friend.address}</p>
-      <p>${friend.email}</p>
-      <p>${friend.phoneNumber}</p>
-      <button class="btn btn-danger delete-btn" data-delete-id=${friend.id}>X</button>
-      <button class="btn btn-info edit-btn" data-edit-id=${friend.id}>Edit</button>
-    </div>
-  `;
+  <div>
+    <h2>${friend.name}</h2>
+    <h3>${friend.relationship}</h3>
+    <p>${friend.address}</p>
+    <p>${friend.email}</p>
+    <p>${friend.phoneNumber}</p>
+    <button class="btn btn-danger delete-btn" data-delete-id=${friend.id}>Delete friend</button>
+    <button class="btn btn-secondary edit-btn" data-edit-id=${friend.id}>Edit friend</button>
+    <div class="holiday-container">${holidayStringBuilder(holidays)}</div>
+  </div>`;
   $('#single-container').html(friendString);
 };
 
 const getSingleFriend = (e) => {
-  // firebase id
+  // need firebase id
   const friendId = e.target.dataset.dropdownId;
-  friendsData.getSingleFriend(friendId)
-    .then((singleFriend) => {
-      printSingleFriend(singleFriend);
-    })
+  const uid = authHelpers.getCurrentUid();
+  friendsData.getSingleFriend(friendId).then((singleFriend) => {
+    // below functions are how we get the holidays to display on friend
+    holidayFriendsData.getHolidayIdsForFriend(friendId).then((holidayIds) => {
+      // console.log('holidayIds', holidayIds);
+      // holidayIds is an array of all the holidays that the friend is attending
+      holidaysData.getHolidaysByArrayOfIds(uid, holidayIds).then((holidays) => {
+        printSingleFriend(singleFriend, holidays);
+      });
+    });
+    // const holidayIds = ['holiday1'];
+    // const holidays = ['a', 'b', 'c'];
+  })
     .catch((error) => {
       console.error('error in getting one friend', error);
     });
 };
 
 const buildDropdown = (friendsArray) => {
-  let dropdown = `<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Pick a Friend
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
+  let dropdown = `
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Select a friend
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
   if (friendsArray.length) {
     friendsArray.forEach((friend) => {
       dropdown += `<div class="dropdown-item get-single" data-dropdown-id=${friend.id}>${friend.name}</div>`;
@@ -43,7 +63,6 @@ const buildDropdown = (friendsArray) => {
   } else {
     dropdown += '<div class="dropdown-item">You have no friends.</div>';
   }
-
   dropdown += '</div></div>';
   $('#dropdown-container').html(dropdown);
 };
@@ -55,20 +74,20 @@ const friendsPage = () => {
       buildDropdown(friendsArray);
     })
     .catch((error) => {
-      console.error('error in getting friends', error);
+      console.error('error on friendsPage', error);
     });
 };
 
 const deleteFriend = (e) => {
-  // firebase id
   const idToDelete = e.target.dataset.deleteId;
   friendsData.deleteFriend(idToDelete)
+    // dont need to pass in result because if delete is successful it, value = null
     .then(() => {
       friendsPage();
       $('#single-container').html('');
     })
     .catch((error) => {
-      console.error('error in deleting friend', error);
+      console.error('error on idToDelete', error);
     });
 };
 
